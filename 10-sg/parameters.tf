1993fae20f3649e248ac152e474e12b346df20dc
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-07-12 19:29:03 +0530

    infra

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -25,11 +25,11 @@ resource "aws_ssm_parameter" "mongodb_sg_id" {
 #}
 
 
-#resource "aws_ssm_parameter" "catalogue_sg_id" {
-#  name  = "/${var.project}/${var.environment}/catalogue_sg_id"
-#  type  = "String"
-#  value = module.catalogue.sg_id
-#}
+resource "aws_ssm_parameter" "catalogue_sg_id" {
+  name  = "/${var.project}/${var.environment}/catalogue_sg_id"
+  type  = "String"
+  value = module.catalogue.sg_id
+}
 ###
 #resource "aws_ssm_parameter" "user_sg_id" {
 #  name  = "/${var.project}/${var.environment}/user_sg_id"
